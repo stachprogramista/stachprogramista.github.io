975fa9062f1be904f4e41a55ac914e2cd0bd867e
Stanisław Piekiełko <piekielkostanislaw@gmail.com>
2026-03-30 15:43:35 +0200

    Changed css including color scheme and font. Added missing content

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ function personal_projects_page_script() {
     fetch(`https://api.github.com/users/${username}/repos`)
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             data.forEach(project => {
                 let projectHTML = `<div>
         <p>${project.name}</p>
-        <a href="${project.html_url}" target="_blank">View</a>
+        <a href="${project.html_url}" target="_blank" class="external_link">View</a>
         <br>
       </div>`;
                 main.innerHTML += projectHTML;
@@ -19,7 +20,55 @@ function personal_projects_page_script() {
         })
         .catch(error => console.error(error));
 }
+function contributions_page_script() {
+    const username = 'stachprogramista'
+    const main = document.querySelector('main');
+
+    async function getPublicContributions(username) {
+        const repos = [];
+        let page = 1;
+        while (true) {
+            const res = await fetch(`https://api.github.com/search/issues?q=is:pr+author:${username}+is:merged&type=pr&page=${page}&per_page=100`);
+            const data = await res.json();
+            if (!data.items.length) break;
+            for (const item of data.items) {
+                const repo = item.repository_url.split('/repos/')[1];
+                if (!repos.includes(repo)) repos.push(repo);
+            }
+            page++;
+        }
+        return repos;
+    }
+
+    const loadContributions = async (username) => {
+  try {
+    const contributions = await getPublicContributions(username);
+    
+    if (contributions.length === 0) {
+      main.innerHTML += "<div><p>Nothing here <b>yet</b></p></div>";
+    } else {
+      contributions.forEach(project => {
+        const projectHTML = `<div>
+          <p>${project.name || project}</p>
+          <a href="${project.html_url || `https://github.com/${project}`}" target="_blank" class="external_link">View</a>
+        </div>`;
+        main.innerHTML += projectHTML;
+      });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    main.innerHTML += "<div>Error loading contributions</div>";
+  }
+};
+
+loadContributions(username);
+
+}
 switch (page) {
     case 'personal_projects':
-        personal_projects_page_script()
+        personal_projects_page_script();
+        break;
+    case 'contributions':
+        contributions_page_script()
+        break;
 }
